b1ab5a2ea8c9dbc065ab273381cb6bd511d607f4
Savan Italiya <146214409+Savan-Italiya@users.noreply.github.com>
2025-04-03 10:35:22 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ const {
 
 exports.connect = async () => {
   try {
-    await mongoose.connect("mongodb+srv://admin:admin@ppsllp.zltubwi.mongodb.net/WA_Sender_Institute?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://is:is123@hilance.6ehjiul.mongodb.net/WA_Sender_Institute?retryWrites=true&w=majority");
     console.log("✅ Successfully connected to MongoDB");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
